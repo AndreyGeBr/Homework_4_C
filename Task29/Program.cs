d6ea1bf8e8dedf99f1ee2575b79a676896ebb9f0
Andrey <a89615076005@yandex.ru>
2022-10-03 16:23:50 +0300

    add task29

---
+++ Task29/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// Ограничения:
+// - Должна быть отдельная функция, которая создаёт массив и отдельная функция вывода
+// - Внутри функций (кроме функции вывода) НЕЛЬЗЯ использовать класс Console. Только в основном блоке кода или в функции вывода
+
+
+Random random = new Random();
+int size = 8;
+int[] numbers = new int[size];
+Console.WriteLine ($"Размер массива {size}");
+
+FillArray(numbers);
+PrintArray(numbers);
+
+void FillArray(int[] num)
+{
+    Random random = new Random();
+    for (int i = 0; i < num.Length; i++)
+    {
+        num[i] = random.Next(0, 100);
+    }
+} 
+
+void PrintArray(int[] num)
+{
+    for (int i = 0; i < num.Length; i++)
+    {
+        Console.Write($"{num[i]} ");
+    }
+}
