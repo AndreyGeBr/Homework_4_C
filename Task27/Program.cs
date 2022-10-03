f46194464ca872584f99eb34c519e55042ee1ea3
Andrey <a89615076005@yandex.ru>
2022-10-03 16:14:42 +0300

    add task 27

---
+++ Task27/Program.cs
@@ -0,0 +1,28 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// Ограничения:
+// - Должна быть отдельная функция, которая возвразает результат суммы цифр
+// - Внутри функций НЕЛЬЗЯ использовать класс Console. Только в основном блоке кода
+
+Console.Clear();
+ 
+Console.Write("Введите число: ");
+int number = Math.Abs(int.Parse(Console.ReadLine()!));
+
+int result = 0;
+SumNumbers ();
+
+Console.WriteLine(result);
+
+
+
+void SumNumbers ()
+{
+    while (number > 0)
+    {
+        int digit = number % 10;
+        result = result + digit;
+        number = number / 10;
+    }
+}
+
+
